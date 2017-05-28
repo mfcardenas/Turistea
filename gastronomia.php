06ffb60ed62c6753761ec7fa1e294feea96465bf
David Hervás <dahervas@ucm.es>
2017-05-28 02:02:34 +0200

    a ver guapetonios. He subido la tabla que he modificao de la base de datos, el php de la gastronomía que carga e inserta (excepto las fuckin imágenes) y las fotos que pertenecen a la carpeta que también es nueva para que quede mas cuki. Buenas noches

--- gastronomia.php
+++ gastronomia.php
@@ -1,3 +1,94 @@
+<?php
+
+	$db = mysqli_connect("localhost", "root", "" , "turistea");
+
+	$query_nombresG = "SELECT Nombre FROM gastronomia";
+  	$result0 = mysqli_query($db, $query_nombresG);
+  	$nombresG = array();
+
+  	while($row = mysqli_fetch_assoc($result0))
+    { $nombresG[] = $row['Nombre'];}
+
+	//echo "HOLAAAAAAAAAAAAAAAAAA";
+	//echo "$nombresG[0]";
+	//echo "$nombresG[1]";
+
+	$query_direccionG = "SELECT Direccion FROM gastronomia";
+  	$result1 = mysqli_query($db, $query_direccionG);
+  	$direccionG = array();
+
+  	while($row = mysqli_fetch_assoc($result1))
+    { $direccionG[] = $row['Direccion'];}
+
+	//echo "$direccionG[0]";
+	//echo "$direccionG[1]";
+
+
+	$query_telefonoG = "SELECT Telefono FROM gastronomia";
+  	$result2 = mysqli_query($db, $query_telefonoG);
+  	$telefonoG = array();
+
+  	while($row = mysqli_fetch_assoc($result2))
+    { $telefonoG[] = $row['Telefono'];}
+
+	$query_estrellaG = "SELECT `Plato estrella` FROM gastronomia";
+  	$result3 = mysqli_query($db, $query_estrellaG);
+  	$estrellaG = array();
+
+  	while($row = mysqli_fetch_assoc($result3))
+    { $estrellaG[] = $row['Plato estrella'];}
+
+	//echo "$estrellaG[0]";
+	//echo "$estrellaG[1]";
+
+	/*$query_llegadaG = "SELECT Llegada FROM gastronomia";
+  	$result4 = mysqli_query($db, $query_llegadaG);
+  	$llegadaG = array();
+
+  	while($row = mysqli_fetch_assoc($result4))
+    { $llegadaG[] = $row['Llegada'];}*/
+
+	//GUARDAR DATOS DEL FORMULARIO DE AÑADIR
+	error_reporting(E_PARSE);
+
+	$nombreAdd=$_POST['nombrebar'];
+  	$llegarAdd=$_POST['llegadabar'];
+  	$horarioAdd=$_POST['horariobar'];
+  	$direccionAdd=$_POST['localizacionbar1'];
+  	$telefonoAdd=$_POST['telefonobar'];
+  	$estrellaAdd=$_POST['estrellabar'];
+
+  	
+
+  	/*$dir_subida = 'imgGastro/';
+  	$fichero_subido = $dir_subida . basename($_FILES['fichero']['name']);
+	$file_name = basename($_FILES['fichero']['name']);
+	$imageFileType = pathinfo($fichero_subido,PATHINFO_EXTENSION);	
+
+	if($_FILES['fichero']['name']){ echo "HAY ALGO SUBIDO"; }
+
+  	if (move_uploaded_file($_FILES['fichero']['tmp_name'], $fichero_subido)) {
+    	echo "El fichero es válido y se subió con éxito.\n";
+	} else {
+    	echo "¡Posible ataque de subida de ficheros!\n";
+	}
+	var_dump($_FILES);*/
+	/*$currentdir = getcwd();
+	echo "$currentdir";
+
+	$target = $currentdir .'/imgGastro/' . basename($_FILES['fichero']['name']);
+	move_uploaded_file($_FILES['fichero']['tmp_name'], $target);*/
+	
+	
+
+  	if(isset($_POST['submit'])){
+  				
+		//$query_add="INSERT INTO gastronomia (`Nombre`, `Imagen`, `Direccion`, `Telefono`, `Horario`, `Llegada`, `Plato estrella`) VALUES ('$nombreAdd', NULL, '$direccionAdd', '$telefonoAdd', '$horarioAdd', '$llegarAdd', '$estrellaAdd')";
+		//$inserta=mysqli_query($db, $query_add);
+		
+	}
+
+?>
 <!DOCTYPE html>
 <html lang="es">
     <head> 
@@ -63,30 +154,29 @@
 	  					</tr>
 					  	<tr>
 					    	<td>1</td>
-					    	<td><a href="lambuzo.php">Lambuzo</a></td>
+					    	<td><a href="lambuzo.php"><?php echo "$nombresG[0]"; ?></a></td>
 					    	<td>El bar Lambuzo es uno de esos sitios que se nota que está montado con cariño. En Madrid existen dos locales de la misma compañía, uno en la calle Ponzano y otro en las Conchas. Para este post nos centraremos en el bar situado en la calle Ponzano que es el más concurrido en la ciudad. Este bar es ideal para ir de tapas con un toque andaluz. Podrás viajar directamente a Cadiz a través de las maravillosas tapas que este fantástico bar ofrece.</td>
-					    	<td>CHAMBERÍ 
-						    	Calle Ponzano, 8
-								28010 Madrid 
-								Madrid, España</td>
+					    	<td><?php
+					    		echo "$direccionG[0]"; 
+					    	?></td>
 					    	<td>BRUH</td>
 					  	</tr>
 					  	<tr>
 					    	<td>2</td>
-					    	<td><a href="elMandil.php">El Mandil</a></td>
+					    	<td><a href="elMandil.php"><?php echo "$nombresG[1]"; ?></a></td>
 					    	<td>El Mandil es un restaurante situado en pleno corazón de Chueca. La decoración es rústica y elegante mezclandolo con un toque clásico a través de una enorme pared de ladrillo que recorre todo el local</td>
-					    	<td>C/ Colón 5
-								28040 Madrid 
-								Madrid, España</td>
+					    	<td><?php
+					    		echo "$direccionG[1]"; 
+					    	?></td>
 					    	<td>BRUH</td>
 					  	</tr>
 					  	<tr>
 					    	<td>3</td>
-					    	<td><a href="montesDeGalicia.php">Los Montes de Galicia</a></td>
+					    	<td><a href="montesDeGalicia.php"><?php echo "$nombresG[2]"; ?></a></td>
 					    	<td>Jose Espasandín, empresario y restaurador propietario de Los Montes de Galicia desde 1997 ha sido fiel a su compromiso con los comensales que han pasado por el restaurante de Azcona, 46. El concepto de Montes de Galicia es muy sencillo. Consiste en ofrecer una experiencia gastronómica completa, desde un picoteo en barra con vinos de prestigio por copas, variedad de recetas tanto clásicas como modernas y coctelería de autor. Y todo ello, en un entorno agradable y rodeados por un servicio que transmita pasión y ganas de hacer disfrutar al cliente.</td>
-					    	<td>Calle Azcona, 46
-								28028 Madrid
-								Madrid, España</td>
+					    	<td><?php
+					    		echo "$direccionG[2]"; 
+					    	?></td>
 					    	<td>BRUH</td>
 					  	</tr>
 					</table>
@@ -96,18 +186,17 @@
 					<hr class="featurette-divider">
 					<div class="row">
 						<div class="col-lg-3">
-							<a href="lambuzo.php"> <h2> Lambuzo </h2> </a>
-							<a href="lambuzo.php"> <img src= "img/lambuzo.jpg" alt="Lambuzo"> </a> 
+							<a href="lambuzo.php"> <h2> <?php echo "$nombresG[0]"; ?> </h2> </a>
+							<a href="lambuzo.php"> <img src= "imgGastro/lambuzo.jpg" alt="Lambuzo"> </a> 
 						</div>
 						<div class="col-lg-9">
 							<p class="subtitulo"> Localización: </p>
-					          	Calle Azcona, 46 <br>
-					          	28028 Madrid Madrid <br>
-					          	España <br><br>
+					          	<?php echo "$direccionG[0]"; ?> <br>
+					          	
 				        	<p class="subtitulo"> Teléfono: </p>
-				          		913552786 <br>
+				          		<?php echo "$telefonoG[0]"; ?> <br>
 				          	<p class="subtitulo"> Plato estrella: </p>
-				          		Espuma de Yogurt <br>
+				          		<?php echo "$estrellaG[0]"; ?> <br>
 				          	<p class=" subtitulo"> Reseñas: </p>
 				          		BRUH <br> <!--ESTO FALTA POR IMPLEMENTAR-->
 				        </div>
@@ -118,19 +207,17 @@
 						<div class="col-lg-6"></div>
 						<div class="col-lg-3"> 
 							<p class="subtitulo"> Localización: </p>
-					          	Calle Colón, 5<br>
-								28040 Madrid Madrid<br>
-								España<br><br>
+					          	<?php echo "$direccionG[1]"; ?> <br>
 				        	<p class="subtitulo"> Teléfono: </p>
-				          		915316255 <br>
+				          		<?php echo "$telefonoG[1]"; ?> <br>
 				          	<p class="subtitulo"> Plato estrella: </p>
-				          		Calamares con cebolla confitada y alioli <br>
+				          		<?php echo "$estrellaG[1]"; ?> <br>
 				          	<p class=" subtitulo"> Reseñas: </p>
 				          		BRUH <br> <!--ESTO FALTA POR IMPLEMENTAR-->
 				         </div>
 						<div class="col-lg-3">
-							<a href="elMandil.php"> <h2> El Mandil </h2> </a>
-							<a href="elMandil.php"> <img src= "img/elMandil.jpg" alt="Mandil"> </a> 
+							<a href="elMandil.php"> <h2> <?php echo "$nombresG[1]"; ?> </h2> </a>
+							<a href="elMandil.php"> <img src= "imgGastro/elMandil.jpg" alt="Mandil"> </a> 
 						</div>
 						<div class="col-lg-9">
 							
@@ -140,18 +227,16 @@
 				    <hr class="featurette-divider">
 					<div class="row">
 						<div class="col-lg-3">
-							<a href="montesDeGalicia.php"> <h2> Los Montes de Galicia </h2> </a>
-							<a href="montesDeGalicia.php"> <img src= "img/losMontesDeGalicia.jpg" alt="Los Montes de Galicia"> </a> 
+							<a href="montesDeGalicia.php"> <h2> <?php echo "$nombresG[2]"; ?> </h2> </a>
+							<a href="montesDeGalicia.php"> <img src= "imgGastro/losMontesDeGalicia.jpg" alt="Los Montes de Galicia"> </a> 
 						</div>
 						<div class="col-lg-9">
 							<p class="subtitulo"> Localización: </p>
-					          	Calle Azcona, 46 <br>
-					          	28028 Madrid Madrid <br>
-					          	España <br><br>
+					          	<?php echo "$direccionG[2]"; ?> <br>
 				        	<p class="subtitulo"> Teléfono: </p>
-				          		913552786 <br>
+				          		<?php echo "$telefonoG[2]"; ?> <br>
 				          	<p class="subtitulo"> Plato estrella: </p>
-				          		Espuma de Yogurt <br>
+				          		<?php echo "$estrellaG[2]"; ?> <br>
 				          	<p class=" subtitulo"> Reseñas: </p>
 				          		BRUH <br> <!--ESTO FALTA POR IMPLEMENTAR-->
 				        </div>
@@ -182,28 +267,31 @@
 				      	     <p> Introduce el nombre: </p>
                              <input type="text" class="form-control" placeholder="Nombre del lugar" name="nombrebar" aria-describedby="basic-addon2" required>
                             <br>
-                            <p>Introduce una imagen: </p>
-                            <input type="file" name="imagenGastronomia" required>
-                            <br>
-				      	     <p> Introduzca una descripción acerca del mismo: </p>
-				      	     <textarea class="form-control" name="descripcionbar" rows="3" placeholder="Escriba aquí la descripcion"></textarea>
+                            <label for="fichero" class="label">Seleccione una imagen: </label>
+				            <div class="input-group">				             
+				                <input id="fichero" name="fichero" type="file" multiple class="file-loading">
+				            </div>
+				            <br>
+				      	     <p> Introduzca una forma de llegar: </p>
+				      	     <textarea class="form-control" name="llegadabar" rows="3" placeholder="Escriba aquí la ruta"></textarea>
 						    <br>
 						     <p> Introduce la dirección: </p>
 						     <input type="text" class="form-control" placeholder="Calle, Nº" name="localizacionbar1" aria-describedby="basic-addon2" required>
-							 <input type="text" class="form-control" placeholder="Localidad, Provincia" name="localizacionbar2" aria-describedby="basic-addon2" required>
-							 <input type="text" class="form-control" placeholder="País" name="localizacionbar3" aria-describedby="basic-addon2" required>
+						     <p> Introduce algún horario: </p>
+							 <input type="text" class="form-control" placeholder="L hh:mm:ss (apertura) hh:mm:ss (cierre) M hh:mm:ss (apertura) ...." name="horariobar" aria-describedby="basic-addon2">
+							 <!--<input type="text" class="form-control" placeholder="País" name="localizacionbar3" aria-describedby="basic-addon2">-->
 							<br>
 							 <p> Introduce el teléfono de contacto: </p>
 							 <input type="text" class="form-control" placeholder="Teléfono" name="telefonobar" aria-describedby="basic-addon2">
 							<br>
 							 <p> Introduce el plato estrella: </p>
-						     <input type="text" class="form-control" placeholder="Plato estrella" name="platoestrellabar" aria-describedby="basic-addon2">
+						     <input type="text" class="form-control" placeholder="Plato estrella" name="estrellabar" aria-describedby="basic-addon2">
 
 							<br>
 
 
 
-				      	<button type="submit" class="btn btn-default centrado">Añadir</button>
+				      	<button type="submit" class="btn btn-default centrado" name="submit" value="Upload">Añadir</button>
 						  		<button type="reset" class="btn btn-default ">Borrar</button></center>
                      
                     </div>
